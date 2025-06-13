bed2e4c4a421d1b44e1960bfa06dc1d6abe56528
gulapsagul <gulapsagul@gmail.com>
2025-06-14 02:24:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Board from './componets/Board'
+import "./app.css" 
+
+const App = () => {
+
+  return (
+    <>
+    
+    <div className='win'>
+    <h1>PLAY THE GAME</h1>   
+</div> 
+    <div className='App'>
+
+
+
+
+
+      <Board/>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
